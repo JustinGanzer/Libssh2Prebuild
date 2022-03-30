f55344687fd5d3fe8fab11e2b35d1f0381d250cd
JustinGanzer <fleischgurke@gmail.com>
2022-03-30 10:39:15 +0200

    Uses own prebuilt xcframework

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
     ],
     targets: [
         .binaryTarget(name: "CSSH",
-                      url: "https://github.com/DimaRU/Libssh2Prebuild/releases/download/1.10.0+OpenSSL_1_1_1l/CSSH-1.10.0+OpenSSL_1_1_1l.xcframework.zip",
-                      checksum: "a4aff717265efc34f494618a489fdcaec7f50cf770343449e72113cf794c0ca5")
+                      url: "https://github.com/JustinGanzer/Libssh2Prebuild/releases/download/1.10.0%2BOpenSSL_1_1_1l/CSSH-1.10.0+OpenSSL_1_1_1l.xcframework.zip",
+                      checksum: "3bd131f4f930439c2846d6ecb6862893778d41c62f866829a45a92ad1e3d472a")
     ]
 )
